f46911a10ff2cfbe9367fc712aa077f875f8ed0b
Md Tarek Hossain Tusher <57767316+tarektusher@users.noreply.github.com>
2023-08-03 17:26:26 +0600

    Today is second Class.Today's Lesson: how to create an user and store an array. how to get an all users. how to get a user by an specfic id

--- server.js
+++ server.js
@@ -1,13 +1,45 @@
 //? Package List
 const express = require('express');
-const boydParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 
 const app = express();
-app.use(boydParser.json());
+app.use(bodyParser.json());
 const PORT = 3000;
+
+//? Create a object 
+let users = [];
+let cc=0;
+
+//? API to check connection
+app.get('/',(req,res) =>{
+     res.json({message: 'Welcome to my app'});
+})
+
+//? API to create a User
+app.post('/users',(req,res) => {
+     const user = req.body;
+     user.id = ++cc;
+     users.push(user);
+     res.status(201).json(user);
+})
+
+//? API to get Users 
+app.get('/users',(req,res) =>{
+     res.status(200).json(users);
+})
+//? API to get specfic user
+app.get('/users/:id',(req,res)=>{
+     const id =req.params.id;
+     const user = users.find((u) => u.id ==  id);
+     if(user){
+          res.json(user);
+     }
+     else{
+          res.status(404).json({message:"User Not Found"});
+     }
+})
 
 app.listen(PORT,() =>{
      console.log(`Server is running on PORT ${PORT}`);
 })
-//? Tarek
